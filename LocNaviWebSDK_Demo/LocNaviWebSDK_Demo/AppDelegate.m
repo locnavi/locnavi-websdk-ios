aa331dbf776c6239e8db6b9aecb9d959e8a2e36c
zhangty <673534349@qq.com>
2020-07-07 23:50:18 +0800

    增加范例

--- LocNaviWebSDK_Demo/LocNaviWebSDK_Demo/AppDelegate.m
+++ LocNaviWebSDK_Demo/LocNaviWebSDK_Demo/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import <LocNaviWebSDK/LocNaviWebSDK.h>
+#import <XJLocation/XJLocation.h>
 
 @interface AppDelegate ()
 
@@ -19,6 +20,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [LocNaviMapService setAppKey:@"nqB6HPIU2C"];
+    [XJLocationService initServices: @"nqB6HPIU2C"];
     
     return YES;
 }
--- LocNaviWebSDK_Demo/LocNaviWebSDK_Demo/ViewController.m
+++ LocNaviWebSDK_Demo/LocNaviWebSDK_Demo/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 #import <LocNaviWebSDK/LocNaviWebSDK.h>
+#import <XJLocation/XJLocation.h>
 
-@interface ViewController ()
+@interface ViewController ()<XJmapLocationDelegate>
+
+@property (nonatomic, strong)XJmapLocationManger *locationManger;
 
 @end
 
@@ -18,21 +21,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
     [btn setBackgroundColor:[UIColor redColor]];
-    [btn setTitle:@"进入地图" forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(onShowMap) forControlEvents:UIControlEventTouchUpInside];
-    btn.frame = CGRectMake(100, 100, 100, 40);
+    [btn setTitle:@"启动引擎" forState:UIControlStateNormal];
+    [btn setTitle:@"停止引擎" forState:UIControlStateSelected];
+    [btn addTarget:self action:@selector(onControlEngine:) forControlEvents:UIControlEventTouchUpInside];
+    btn.frame = CGRectMake(50, 100, 100, 40);
     [self.view addSubview:btn];
+    
+    
+    UIButton *btn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn2 setBackgroundColor:[UIColor redColor]];
+    [btn2 setTitle:@"进入地图" forState:UIControlStateNormal];
+    [btn2 addTarget:self action:@selector(onShowMap) forControlEvents:UIControlEventTouchUpInside];
+    btn2.frame = CGRectMake(170, 100, 100, 40);
+    [self.view addSubview:btn2];
+    
+    self.locationManger = [XJmapLocationManger new];
+    self.locationManger.delegate = self;
+}
+
+- (void)onControlEngine:(UIButton *)btn {
+    if (btn.selected) {
+        [self.locationManger stopLocatingEngine];
+    } else {
+        [self.locationManger startLocationEngine:@"HHrzBwF5dY"];
+    }
+    btn.selected = !btn.selected;
 }
 
 - (void)onShowMap {
     NSLog(@"sss");
     LocNaviWebViewController *vc = [[LocNaviWebViewController alloc] initWithMapId:@"HHrzBwF5dY"];
-    vc.view.backgroundColor = [UIColor blueColor];
     vc.modalPresentationStyle = UIModalPresentationFullScreen;
     [self presentViewController:vc animated:YES completion:nil];
+}
+
+- (void)xjmapLocationManager:(XJmapLocationManger *_Nullable)manager didUpdateLocation:(XJLocationInfo *_Nullable)location {
+    NSLog(@"%@", location.inThisMap ? @"在院内" : @"不在院内");
+    NSLog(@"%lf %lf", location.coordinate.longitude, location.coordinate.latitude);
+    NSLog(@"%@", location.floor);
+    NSLog(@"%@", location.floorDescription);
+    NSLog(@"%@", location.strDesc);
 }
 
 
